7d94b408d9ebd9a2ef7e6aa52b6a8f2ecb74351f
VinciVelasco <vincivelasco@gmail.com>
2023-08-30 11:21:34 -0700

    Refactored views+urls to be easier to work with and implemented ability to remove clothing.

--- clothingSite/outfitGenerator/urls.py
+++ clothingSite/outfitGenerator/urls.py
@@ -5,10 +5,9 @@ app_name = "outfitGenerator"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wardrobe", views.wardrobe_view, name="wardrobe"),
-    path("add-top", views.add_top_clothing_view, name="add-top"),
-    path("add-bottom", views.add_bottom_clothing_view, name="add-bottom"),
-    path("add-shoes", views.add_shoes_view, name="add-shoes"),
+    path("wardrobe/", views.wardrobe_view, name="wardrobe"),
+    path("add/<str:clothing_type>/", views.add_clothing_view, name="add_clothing"),
+    path("remove/<str:clothing_type>/<int:pk>/", views.remove_clothing_view, name="remove_clothing"),
     path("generate-outfit", views.generate_outfit_view, name="generate_outfit"),
     path("generate-week", views.generate_week_view, name="generate_week"),
 ]
--- clothingSite/outfitGenerator/views.py
+++ clothingSite/outfitGenerator/views.py
@@ -25,42 +25,21 @@ def wardrobe_view(request):
         "shoeform": forms.ShoeForm
     })
 
-def add_top_clothing_view(request):
+def add_clothing_view(request, clothing_type):
     if request.method == "POST":
-        form = forms.TopClothingForm(request.POST)
-
+        form = get_form(request, clothing_type)
         if form.is_valid():
-            # Add current logged-in user to the form data
             if request.user.is_authenticated:
                 form = form.save(commit=False)
-                form.user = request.user
+                form.user = request.user # Add current logged-in user to the form data
                 form.save()
 
     return HttpResponseRedirect(reverse("outfitGenerator:wardrobe"))
 
-def add_bottom_clothing_view(request):
+def remove_clothing_view(request, clothing_type, pk):
     if request.method == "POST":
-        form = forms.BottomClothingForm(request.POST)
-
-        if form.is_valid():
-            # Add current logged-in user to the form data
-            if request.user.is_authenticated:
-                form = form.save(commit=False)
-                form.user = request.user
-                form.save()
-
-    return HttpResponseRedirect(reverse("outfitGenerator:wardrobe"))
-
-def add_shoes_view(request):
-    if request.method == "POST":
-        form = forms.ShoeForm(request.POST)
-
-        if form.is_valid():
-            # Add current logged-in user to the form data
-            if request.user.is_authenticated:
-                form = form.save(commit=False)
-                form.user = request.user
-                form.save()
+        obj = get_clothing_obj(clothing_type, pk)
+        obj.delete()
 
     return HttpResponseRedirect(reverse("outfitGenerator:wardrobe"))
 
@@ -69,3 +48,33 @@ def generate_outfit_view(request):
 
 def generate_week_view(request):
     return render(request, "outfitGenerator/generate_week.html")
+
+# Helper functions
+
+# Return correct form given clothing type
+def get_form(request, clothing_type):
+    if clothing_type == "top":
+        form = forms.TopClothingForm(request.POST)
+    elif clothing_type == "bottom":
+        form = forms.BottomClothingForm(request.POST)
+    elif clothing_type == "shoes":
+        form = forms.ShoeForm(request.POST)
+    else:
+        print("Invalid URL") # potentially add a 404 not found page that is sent back
+        return None
+
+    return form
+
+# Return correct clothing object given clothing type and pk number
+def get_clothing_obj(clothing_type, pk):
+    if clothing_type == "top":
+        obj = models.TopClothing.objects.get(id=pk)
+    elif clothing_type == "bottom":
+        obj = models.BottomClothing.objects.get(id=pk)
+    elif clothing_type == "shoes":
+        obj = models.Shoe.objects.get(id=pk)
+    else:
+        print("Invalid URL") # potentially add a 404 not found page that is sent back
+        return None
+
+    return obj
